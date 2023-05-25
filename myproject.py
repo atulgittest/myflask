6ef8428a839a9e6a4fbd98262a1e4e25edbd0f2a
atulgittest <atul.atulbhandari@gmail.com>
2023-05-25 15:17:05 +0530

    add frontend

--- myproject.py
+++ myproject.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello Atul You can success full deploy cicd with jenkins
-                                   Yes Its working fine !</h1>"
+    return "<h1 style='color:blue'>Hello All Now I am Sucessfull Deploy Flask!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
